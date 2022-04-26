d44058ff0cbaf1b91b4b1e82d2515f268ee2ce8f
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-27 03:33:42 +0530

    Added: Password Should have atleast 1 Upper Case

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 /*
  * @ author: Nikhil Deshmukh
  * As a User need to follow pre-defined Password rules.
- * Rule1 – minimum 8 Characters
+ * Rule2 – Should have at least 1 Upper Case.
  */
 public class UserRegistrationMain {
 	public static void main(String[] args) {
