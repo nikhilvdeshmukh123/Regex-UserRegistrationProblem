252cff06511b6eac8a62ad0b6260879cecbb621b
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-27 00:11:03 +0530

    Added: User need to Enter Valid Last name

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -1,14 +1,15 @@
 package com.bridgelabz;
 /*
  * @ author: Nikhil Deshmukh
- * User need to enter a valid First Name:
- * - First name starts with Cap and has minimum 3 characters
+ * User need to enter a valid Last Name:
+ * - Last name starts with Cap and has minimum 3 characters
  */
 public class UserRegistrationMain {
 	public static void main(String[] args) {
 		// Create object of class
 		ValidateUserInfo validateUserInfo = new ValidateUserInfo();
-		// Method calling to check Firstname
+		// Method calling 
 		validateUserInfo.checkFirstName();
+		validateUserInfo.checkLastName();
 	}
 }
