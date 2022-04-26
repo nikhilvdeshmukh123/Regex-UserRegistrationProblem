991a472578489cfed7d66b069c0debe98d412c05
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-27 03:06:11 +0530

    Added: User need to add mobile Number Pre-Defined Format

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -1,8 +1,9 @@
 package com.bridgelabz;
 /*
  * @ author: Nikhil Deshmukh
- * As a User need to enter a valid email - E.g. abc.xyz@bl.co.in
- * - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+ * As a User need to follow pre defined Mobile Format.
+ * Mobile Format - E.g. 91 9919819801 
+ * - Country code follow by space and 10 digit number
  */
 public class UserRegistrationMain {
 	public static void main(String[] args) {
